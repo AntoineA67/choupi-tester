05ddb85f7c91e0a7158f4941d4ee72331d2823ac
Antoine <adamy.antoine@gmail.com>
2021-12-12 20:05:03 +0100

    minimal

--- choupi-tester.py
+++ choupi-tester.py
@@ -6,7 +6,7 @@
 #    By: arangoni <arangoni@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/12/12 14:52:12 by arangoni          #+#    #+#              #
-#    Updated: 2021/12/12 18:30:06 by arangoni         ###   ########.fr        #
+#    Updated: 2021/12/12 20:03:03 by arangoni         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -73,6 +73,8 @@ def doArgs(argList):
 	parser.add_argument('-a', "--average", action="store", dest="avg", type=int, help="number of tests to average from - default 10", default=10)
 	parser.add_argument('-c', "--check", action="store_true", dest="check", help="checks push_swap output with ./checker")
 	parser.add_argument('-r', "--rectangle", action="store_true", dest="rectangle", help="adds rectangle to plot")
+	parser.add_argument('-m', "--minimal", action="store_true", dest="minimal", help="only tests 0, 1, 3, 5, 100, 500 with 300 tests per value (default, can be changed wiht -a)")
+
 
 	return parser.parse_args(argList)
 
@@ -87,8 +89,9 @@ def color_range(value, lst):
 def main():
 	print(CVIOLET + "   ___ _                  _   _____       _           \n  / __| |_  ___ _  _ _ __(_) |_   _|__ __| |_ ___ _ _ \n | (__| ' \/ _ \ || | '_ \ |   | |/ -_|_-<  _/ -_) '_|\n  \___|_||_\___/\_,_| .__/_|   |_|\___/__/\__\___|_|  \n                    |_|                               " + CEND)
 
-	args = doArgs(sys.argv[1:])
+	
 
+	args = doArgs(sys.argv[1:])
 	AVERAGE = args.avg
 	if len(args.integers) == 1:
 		args.integers.append(args.integers[0])
@@ -99,7 +102,40 @@ def main():
 	START = args.integers[0]
 	END = args.integers[1]
 
-	print(\
+	
+	
+	startTime = float(time.time())
+
+	tab = []
+	if (args.minimal):
+		if AVERAGE == 10:
+			AVERAGE = 300
+		print(\
+	CBLUE2 + "Options:\n" + CBEIGE2 + "\
+	Minimal version\n\
+	Average: {}\n\
+		".format(color_range(AVERAGE, [10, 100])))
+		minimal = [1, 3, 5, 100, 500]
+		if args.verbose:
+			print(CURL + CBLUE2 + "{:^20}{:^20}{:^20}\n".format("Average", "Max" if 1 else '', "Size") + CEND)
+		for index, i in enumerate(minimal):
+			tab.append([])
+			for j in range(AVERAGE):
+				l = [x for x in range(i)]
+				random.shuffle(l)
+				if args.verbose == 2:
+					print(l)
+				prog = subprocess.check_output(["./push_swap"] + [str(x) for x in l], text=1)
+				if args.verbose == 2:
+					print(prog.split('\n'))
+				if (subprocess.check_output(["./checker"] + [str(x) for x in l], input=prog, text=True) == 'KO\n'):
+					print(CRED + "KO" + CEND)
+					exit()
+				tab[index].append(len(prog.split('\n')))
+			if args.verbose:
+				print(CBEIGE2 + "{:^20.1f}{:^20}{:^20}".format(avg(tab[index]), max(tab[index]), i) + CEND)
+	else:
+		print(\
 	CBLUE2 + "Options:\n" + CBEIGE2 + "\
 	Average: {}\n\
 	Range: {}\n\
@@ -110,92 +146,81 @@ def main():
 			CVIOLET2 + str(START) + " - " + str(END) + CBEIGE2,\
 			CGREEN + "Checking solutions" + CBEIGE2 if args.check else CRED2 + "Only checking size, could be KO" + CBEIGE2,\
 			color_range(args.verbose, (1, 3))))
-
-	tab = []
-	if args.verbose:
-		print(CURL + CBLUE2 + "{:^20}{:^20}{:^20}\n".format("Average", "Max" if 1 else '', "Size") + CEND)
-	startTime = float(time.time())
-	for i in range(START, END + 1):
-		tab.append([])
-		for j in range(0, AVERAGE):
-			l = [x for x in range(0, i)]
-			random.shuffle(l)
-			if args.verbose == 2:
-				print(l)
-			prog = subprocess.check_output(["./push_swap"] + [str(x) for x in l], text=1)
-			if args.verbose == 2:
-				print(prog.split('\n'))
-			if args.check and (subprocess.check_output(["./checker"] + [str(x) for x in l], input=prog, text=True) == 'KO\n'):
-				print(CRED + "Non!" + CEND)
-				exit()
-			tab[i - START].append(len(prog.split('\n')))
 		if args.verbose:
-			print(CBEIGE2 + "{:^20.1f}{:^20}{:^20}".format(avg(tab[i - START]), max(tab[i - START]), i) + CEND)
+			print(CURL + CBLUE2 + "{:^20}{:^20}{:^20}\n".format("Average", "Max" if 1 else '', "Size") + CEND)
+		for i in range(START, END + 1):
+			tab.append([])
+			for j in range(0, AVERAGE):
+				l = [x for x in range(0, i)]
+				random.shuffle(l)
+				if args.verbose == 2:
+					print(l)
+				prog = subprocess.check_output(["./push_swap"] + [str(x) for x in l], text=1)
+				if args.verbose == 2:
+					print(prog.split('\n'))
+				if args.check and (subprocess.check_output(["./checker"] + [str(x) for x in l], input=prog, text=True) == 'KO\n'):
+					print(CRED + "KO" + CEND)
+					exit()
+				tab[i - START].append(len(prog.split('\n')))
+			if args.verbose:
+				print(CBEIGE2 + "{:^20.1f}{:^20}{:^20}".format(avg(tab[i - START]), max(tab[i - START]), i) + CEND)
 
-	print("{}Finished {} tests in {:.1f} seconds".format(CURL + CBLUE2 + ' ' * 60 + CBLUE2 + '\n\n' if args.verbose else '', AVERAGE * (END - START + 1), time.time() - startTime))
+	print("{}Finished {} tests in {:.1f} seconds".format(CURL + CBLUE2 + ' ' * 60 + CEND + CBLUE2 + '\n\n' if args.verbose else '', AVERAGE * (END - START + 1), time.time() - startTime))
+	if END - START > 1 and not args.minimal:
+		l = [i for i in range(START, END + 1)]
+		fig, ax = plt.subplots()
+		maxi_list = list(map(max, tab))
+		mini_list = list(map(min, tab))
+		ax.fill_between(l, list(map(min, tab)), maxi_list, alpha=.5, linewidth=2)
+		ax.plot(l, list(map(avg, tab)))
 
-	l = [i for i in range(START, END + 1)]
-	#steps = [3] * 3 + [12] * 2 + [1500] * 95 + [11500] * 400
-	#print(steps)
+		color_map = [0] * len(l)
+		for i, n in enumerate(maxi_list):
+			if i + START <= 3:
+				if n <= 3:
+					color_map[i] = 5
+			elif i + START <= 5:
+				if n <= 12:
+					color_map[i] = 5
+			elif i + START <= 100:
+				if n <= 700:
+					color_map[i] = 5
+				elif n <= 900:
+					color_map[i] = 4
+				elif n <= 1100:
+					color_map[i] = 3
+				elif n <= 1300:
+					color_map[i] = 2
+				elif n <= 1500:
+					color_map[i] = 1
+			elif i + START <= 500:
+				if n <= 5500:
+					color_map[i] = 5
+				elif n <= 7000:
+					color_map[i] = 4
+				elif n <= 8500:
+					color_map[i] = 3
+				elif n <= 1000:
+					color_map[i] = 2
+				elif n <= 11500:
+					color_map[i] = 1
+		map_limit = ListedColormap(
+			colors=['#F94144', '#F3722C', '#F9C74F', '#90BE6D', '#43AA8B', '#577590'],
+			name='map_limit',
+			N=6
+		)
 
-	fig, ax = plt.subplots()
-	# ax.axhspan(0, 3, facecolor='#45ba740f')
-	# ax.axhspan(0, 3, facecolor='#45ba740f')
-	# ax.axhspan(0, 3, facecolor='#45ba740f')
-	maxi_list = list(map(max, tab))
-	mini_list = list(map(min, tab))
-	ax.fill_between(l, list(map(min, tab)), maxi_list, alpha=.5, linewidth=2)
-	ax.plot(l, list(map(avg, tab)))
-
-	color_map = [0] * len(l)
-	for i, n in enumerate(maxi_list):
-		if i + START <= 3:
-			if n <= 3:
-				color_map[i] = 5
-		elif i + START <= 5:
-			if n <= 12:
-				color_map[i] = 5
-		elif i + START <= 100:
-			if n <= 700:
-				color_map[i] = 5
-			elif n <= 900:
-				color_map[i] = 4
-			elif n <= 1100:
-				color_map[i] = 3
-			elif n <= 1300:
-				color_map[i] = 2
-			elif n <= 1500:
-				color_map[i] = 1
-		elif i + START <= 500:
-			if n <= 5500:
-				color_map[i] = 5
-			elif n <= 7000:
-				color_map[i] = 4
-			elif n <= 8500:
-				color_map[i] = 3
-			elif n <= 1000:
-				color_map[i] = 2
-			elif n <= 11500:
-				color_map[i] = 1
-	print(color_map)
-	map_limit = ListedColormap(
-		colors=['#F94144', '#F3722C', '#F9C74F', '#90BE6D', '#43AA8B', '#577590'],
-		name='map_limit',
-		N=6
-	)
-
-	ax.scatter(l, maxi_list, c=color_map, cmap=map_limit)
-	ax.autoscale(False)
-	if args.rectangle:
-		limits = (((0, 0), 4, 3), ((4, 0), 2, 12), ((6, 0), 95, 1500), ((101, 0), 400, 11500))
-		for limit in limits:
-			ax.add_patch(
-				Rectangle(limit[0], limit[1], limit[2], facecolor='#45ba7488')
-			)
-	ax.set(xlabel='Size (n)', ylabel='Operations ({} random tests / size)'.format(AVERAGE),
-		title='# operations, range {} - {}'.format(START, END))#, xlim=(START - START / 100 - 1, END + START / 100 + 1), ylim=(min(mini_list) - min(mini_list) / 10 - 1, max(maxi_list) + min(mini_list) / 10 + 1))
-	ax.grid()
-	if (END - START > 1):
+		ax.scatter(l, maxi_list, c=color_map, cmap=map_limit)
+		ax.autoscale(False)
+		if args.rectangle:
+			limits = (((0, 0), 4, 3), ((4, 0), 2, 12), ((6, 0), 95, 1500), ((101, 0), 400, 11500))
+			for limit in limits:
+				ax.add_patch(
+					Rectangle(limit[0], limit[1], limit[2], facecolor='#45ba7488')
+				)
+		ax.set(xlabel='Size (n)', ylabel='Operations ({} random tests / size)'.format(AVERAGE),
+			title='# operations, range {} - {}'.format(START, END))#, xlim=(START - START / 100 - 1, END + START / 100 + 1), ylim=(min(mini_list) - min(mini_list) / 10 - 1, max(maxi_list) + min(mini_list) / 10 + 1))
+		ax.grid()
 		plt.show()
 		fig.savefig("test.png")
 	return
